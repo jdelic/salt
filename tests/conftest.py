ae210881875c79a5b66da9c78fc87bffc408a677
Pedro Algarvio <pedro@algarvio.me>
2020-08-13 21:50:34 +0100

    Revert "XXX"
    This reverts commit 67788c41f2c13a6b8ca3806c8afced8a982f872e.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1036,47 +1036,6 @@ def salt_proxy_factory(salt_factories, salt_master_factory):
     return factory
 
 
-@pytest.fixture(scope="package")
-def salt_master(salt_master_factory):
-    """
-    We override the fixture so that we have the daemon running
-    """
-    with salt_master_factory.started():
-        yield salt_master_factory
-
-
-@pytest.fixture(scope="package")
-def salt_minion(salt_master, salt_minion_factory):
-    """
-    We override the fixture so that we have the daemon running
-    """
-    with salt_minion_factory.started():
-        # Sync All
-        salt_call_cli = salt_minion_factory.get_salt_call_cli()
-        ret = salt_call_cli.run("saltutil.sync_all", _timeout=120)
-        assert ret.exitcode == 0, ret
-        yield salt_minion_factory
-
-
-@pytest.fixture(scope="package")
-def salt_sub_minion(salt_master, salt_sub_minion_factory):
-    """
-    We override the fixture so that we have the daemon running
-    """
-    with salt_sub_minion_factory.started():
-        # Sync All
-        salt_call_cli = salt_sub_minion_factory.get_salt_call_cli()
-        ret = salt_call_cli.run("saltutil.sync_all", _timeout=120)
-        assert ret.exitcode == 0, ret
-        yield salt_sub_minion_factory
-
-
-@pytest.fixture(scope="package")
-def salt_proxy(salt_master, salt_proxy_factory):
-    with salt_proxy_factory.started():
-        yield salt_proxy_factory
-
-
 @pytest.fixture(scope="session")
 def salt_cli(salt_master_factory):
     return salt_master_factory.get_salt_cli()
